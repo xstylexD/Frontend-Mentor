3171b641c0cbd420e9d936c1d6531ab5bc5039a4
xstylexD <caxapz1q@gmail.com>
2026-04-07 18:16:26 +0300

    update: vite.config and deploy

--- faq-accordion-main/Vite.config.js
+++ faq-accordion-main/Vite.config.js
@@ -1,19 +1,3 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' 
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build'
-    ? '/Frontend-Mentor/faq-accordion-main/'
-    : '/',
-
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 1000,
-      ignored: [
-        '**/dist/**',
-        '**/node_modules/**',
-        '**/.git/**'
-      ]
-    }
-  }
-}))
+export default defineConfig({ base: '/Frontend-Mentor/faq-accordion-main/' })
